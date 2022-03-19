214839dd5f71034019bcee9d3651f42917af0d36
omazim <info.software@omazim.com>
2022-03-19 15:57:27 +0100

    Updated constants with image names for jumping skier and jump ramp.

--- src/Constants.ts
+++ src/Constants.ts
@@ -8,7 +8,8 @@ export enum KEYS {
     LEFT = "ArrowLeft",
     RIGHT = "ArrowRight",
     UP = "ArrowUp",
-    DOWN = "ArrowDown"
+    DOWN = "ArrowDown",
+    SPACEBAR = "Space"// New key event code for jumping.
 };
 
 export enum IMAGE_NAMES {
@@ -30,7 +31,19 @@ export enum IMAGE_NAMES {
     RHINO_EAT3 = 'rhinoEat3',
     RHINO_EAT4 = 'rhinoEat4',
     RHINO_CELEBRATE1 = 'rhinoCelebrate1',
-    RHINO_CELEBRATE2 = 'rhinoCelebrate2'
+    RHINO_CELEBRATE2 = 'rhinoCelebrate2',
+    // Images for skier jumping.
+    SKIER_JUMP1 = "skierJump1",
+    SKIER_JUMP2 = "skierJump2",
+    SKIER_JUMP3 = "skierJump3",
+    SKIER_JUMP4 = "skierJump4",
+    SKIER_JUMP5 = "skierJump5",
+    // Images for obstacles (rock and trees)
+    // ROCK_1 = "rock1",
+    // ROCK_2 = "rock2",
+    // TREE_1 = "tree1",
+    // Images for ramp
+    JUMP_RAMP = "ramp"
 };
 
 export const IMAGES: iImage[] = [
@@ -52,7 +65,13 @@ export const IMAGES: iImage[] = [
     { name: IMAGE_NAMES.RHINO_EAT3, url: 'img/rhino_eat_3.png' },
     { name: IMAGE_NAMES.RHINO_EAT4, url: 'img/rhino_eat_4.png' },
     { name: IMAGE_NAMES.RHINO_CELEBRATE1, url: 'img/rhino_celebrate_1.png' },
-    { name: IMAGE_NAMES.RHINO_CELEBRATE2, url: 'img/rhino_celebrate_2.png' }
+    { name: IMAGE_NAMES.RHINO_CELEBRATE2, url: 'img/rhino_celebrate_2.png' },
+    { name: IMAGE_NAMES.SKIER_JUMP1, url: 'img/skier_jump_1.png' },
+    { name: IMAGE_NAMES.SKIER_JUMP2, url: 'img/skier_jump_2.png' },
+    { name: IMAGE_NAMES.SKIER_JUMP3, url: 'img/skier_jump_3.png' },
+    { name: IMAGE_NAMES.SKIER_JUMP4, url: 'img/skier_jump_4.png' },
+    { name: IMAGE_NAMES.SKIER_JUMP5, url: 'img/skier_jump_5.png' },
+    { name: IMAGE_NAMES.JUMP_RAMP, url: 'img/jump_ramp.png' }
 ];
 
 export const ANIMATION_FRAME_SPEED_MS: number = 250;
